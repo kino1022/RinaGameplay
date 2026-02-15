e49d4919ce1fe2406ec906be79763e07145e44c0
kino1022 <132138562+kino1022@users.noreply.github.com>
2026-02-15 22:44:49 +0900

    Update GameplayAbility.cs

--- Runtime/Ability/GameplayAbility.cs
+++ Runtime/Ability/GameplayAbility.cs
@@ -63,10 +63,10 @@ namespace RinaGameplay.Ability {
         }
 
         void CommitCoolDown(IGameplayAbilitySpec spec, AbilitySystemComponent asc) {
-             foreach (var def in CoolDownDefinitions) {
-                 if (def is null) continue;
+            foreach (var def in CoolDownDefinitions) {
+                if (def is null) continue;
                     def.CommitCoolDown(spec, asc);
-             }
+            }
         }
 
         IActiveGameplayAbility CreateInstance();
@@ -95,10 +95,75 @@ namespace RinaGameplay.Ability {
         [LabelText("アビリティタグ")]
         [Tooltip("アビリティ自体に付与されるタグ")]
         protected IGameplayTagContainer _abilityTags;
-        
-        
+
+        [OdinSerialize]
+        [LabelText("アビリティキャンセルタグ")]
+        [Tooltip("このタグが付与されたアビリティは、このアビリティによってキャンセルされる")]
         protected IGameplayTagContainer _cancelAbilityWithTag;
+
+        [OdinSerialize]
+        [LabelText("アビリティブロックタグ")]
+        [Tooltip("このタグが付与されたアビリティは、このアビリティによってブロックされる")]
+        protected IGameplayTagContainer _blockAbilityWithTag;
+
+        [OdinSerialize]
+        [LabelText("アクティブ時付与タグ")]
+        [Tooltip("アビリティがアクティブな間に付与されるタグ")]
+        protected IGameplayTagContainer _activationOwnedTags;
+
+        [OdinSerialize]
+        [LabelText("アクティベーション必要タグ")]
+        [Tooltip("アビリティをアクティベートするために必要なタグ")]
+        protected IGameplayTagRequirements _activationRequiredTags;
+
+        [OdinSerialize]
+        [LabelText("アクティベーションブロックタグ")]
+        [Tooltip("このタグが付与されたアビリティは、このアビリティによってブロックされる")]
+        protected IGameplayTagRequirements _activationBlockedTags;
+
+        [OdinSerialize]
+        [LabelText("コストの定義")]
+        protected List<IAbilityCostDefinition> _costDefinitions = new List<IAbilityCostDefinition>();
+
+        [OdinSerialize]
+        [LabelText("クールダウンの定義")]
+        protected List<IAbilityCoolDownDefinition> _coolDownDefinitions = new List<IAbilityCoolDownDefinition>();
+
+        [OdinSerialize]
+        [LabelText("タスク")]
+        protected List<IAbilityTask> _tasks = new List<IAbilityTask>();
+
+        [OdinSerialize]
+        [LabelText("インスタンス化ポリシー")]
+        protected AbilityInstancingPolicy _instancingPolicy = AbilityInstancingPolicy.NonInstance;
+
+        public IGameplayTagContainer AbilityTags => _abilityTags;
+
+        public IGameplayTagContainer CancelAbilityWithTag => _cancelAbilityWithTag;
+
+        public IGameplayTagContainer BlockAbilityWithTag => _blockAbilityWithTag;
+
+        public IGameplayTagContainer ActivationOwnedTags => _activationOwnedTags;
+
+        public IGameplayTagRequirements ActivationRequiredTags => _activationRequiredTags;
+
+        public IGameplayTagRequirements ActivationBlockedTags => _activationBlockedTags;
+
+        public IReadOnlyList<IAbilityCostDefinition> CostDefinitions => _costDefinitions;
+
+        public IReadOnlyList<IAbilityCoolDownDefinition> CoolDownDefinitions => _coolDownDefinitions;
+
+        public IReadOnlyList<IAbilityTask> Tasks => _tasks;
+
+        public AbilityInstancingPolicy InstancingPolicy => _instancingPolicy;
+
+        public virtual void ActivateAbilityInternal(IGameplayAbilitySpec spec, AbilitySystemComponent asc) {
+            // デフォルトの実装はなし。必要に応じてオーバーライドして使用。
+        }
         
+        public virtual void EndAbilityInternal(IGameplayAbilitySpec spec, AbilitySystemComponent asc, bool isCancel) {
+            // デフォルトの実装はなし。必要に応じてオーバーライドして使用。
+        }
         
         public abstract IActiveGameplayAbility CreateInstance();
     }
